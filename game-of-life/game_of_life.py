1f34379fe24eda68f035d957bab5b7da10bae944
saviorocha <44814299+saviorocha@users.noreply.github.com>
2021-02-09 23:30:37 -0300

    Refatorei o codigo

--- game-of-life/game_of_life.py
+++ game-of-life/game_of_life.py
@@ -7,19 +7,19 @@ import random
     https://www.youtube.com/watch?v=FWSR_7kZuYg
 """
 
-def makeMatrix(cols, rows):
+resolution = 20
+width = 400
+height = 400
+cols = width // resolution
+rows = height // resolution
+
+def make_matrix(cols, rows):
     matrix = np.zeros( (cols, rows) )
     for i in range(cols):
         for j in range(rows):
             matrix[i][j] = random.randint(0, 1)
     return matrix
-
-resolution = 40
-width = 400
-height = 400
-cols = width // resolution
-rows = height // resolution
-current_grid = makeMatrix(cols, rows)
+current_grid = make_matrix(cols, rows)
 
 def setup():
     size(400, 400)
@@ -27,9 +27,10 @@ def setup():
 def draw():
     global current_grid, cols, rows
     
+    next_grid = np.zeros( (cols, rows) )
     background(0)
     
-    # desenha o grid pela primeira vez
+    # desenha o grid
     for i in range(cols):
         for j in range(rows):
             x = i * resolution
@@ -39,37 +40,35 @@ def draw():
                 rect((x, y), resolution, resolution)
                 # circle((x, y), resolution)
 
-    # desenha o grid do estado seguinte
-    next_grid = makeMatrix(cols, rows)
-
-    for i in range(cols):
-        for j in range(rows):
+            next_grid[i][j] = calculate_new_grid(current_grid, i, j)
             
-            state = current_grid[i][j]
-            neighbors_sum = count_neighbors(current_grid, i, j)
-
-            # regras do jogo da vida
-            if state == 0 and neighbors_sum == 3:
-                next_grid[i][j] = 1
-            elif state == 1 and (neighbors_sum < 2 or neighbors_sum > 3):
-                next_grid[i][j] = 0
-            else:
-                next_grid[i][j] = state
-    
     # atualiza o grid para próxima iteração
     current_grid = next_grid
-            
+
+# cria o grid do estado seguinte
+def calculate_new_grid(current_grid, i, j):
+    cell_state = current_grid[i][j]
+    neighbors_sum = count_neighbors(current_grid, i, j)
+
+    # regras do jogo da vida
+    if cell_state == 0 and neighbors_sum == 3:
+        return 1
+    elif cell_state == 1 and (neighbors_sum < 2 or neighbors_sum > 3):
+        return 0
+    else:
+        return cell_state
+                  
 # soma todos os vizinhos de uma celula, excluindo ela propria
 def count_neighbors(grid, x, y):
     sum = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            # usa-se o módulo por causa das celulas nas bordas
-            col = (x + i + cols) % cols
-            row = (y + j + rows) % rows
+            if i != x and j != y:
+                # usa-se o módulo por causa das celulas nas bordas
+                col = (x + i + cols) % cols # (0 - 1 + 10) % 10 = 9
+                row = (y + j + rows) % rows
 
-            sum += grid[col][row]
-    sum -= grid[x][y]
+                sum += grid[col][row]
     return sum
 
 if __name__ == '__main__':
